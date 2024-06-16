32f206b52d79924dbf411e725d7c6c1ca0d63e01
vindiniz <diniz@anspacepay.com>
2024-06-16 11:13:27 -0300

    [FEAT] Addsso

---
+++ src/ISolutions.NewBackProject.SSO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace TempRepo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+}
+
